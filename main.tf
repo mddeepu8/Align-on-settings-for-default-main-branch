c5c2757419f85d1f6d4eaa986cb57ac8e3590d90
mddeepu8 <84235132+mddeepu8@users.noreply.github.com>
2022-05-11 12:48:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,16 +7,27 @@ terraform {
   }
 }
 provider "github" {
-  organization = "philips-internal"
+  organization = "SynergyDevops"
   #version = "~> 4.0"
 }
 
 resource "github_branch_protection_v3" "main_protection" {
   repository     = "${var.repository}"
-  branch          = "main"
-  enforce_admins   = true
+  branch        = "main"
+  enforce_admins = true
+  require_signed_commits = true
+  require_conversation_resolution = true
+  requiredLinearHistory = true
+  allow_force_push = true
+  allow_deletions = true
+  
+  required_status_checks {
+    strict   = false
+    contexts = ["ci/travis"]
+  }
   required_pull_request_reviews {
     dismiss_stale_reviews  = true
     required_approving_review_count = 2
+    require_code_owner_reviews = false
   }
 }
